0a903d48994435dce4599d4298d4043c3ad9bce2
Will Certeza <wcerteza@gmail.com>
2023-05-09 10:14:03 -0500

    hardest part yet. updating handleEnter functin to check winning conditions, added globar array, added variables inside function

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const secretWord = 'EARTH'
+const guesses = []
 let currentGuess = []
 let currentLetter = 1
 let gameOver = false
+
 const guessGrid = document.getElementById('game')
 const messageEl = document.getElementById('message-el')
+const gameBoard = document.getElementById('game')
+const squares = document.querySelectorAll('.square')
 
 const createGameBoard = () => {
-  const gameBoard = document.getElementById('game')
   for (let i = 0; i < 30; i++) {
     const gameSquare = document.createElement('div')
     gameSquare.classList.add('square')
@@ -14,8 +17,6 @@ const createGameBoard = () => {
     gameBoard.appendChild(gameSquare)
   }
 }
-
-createGameBoard()
 
 const handleDelete = () => {
   if (!gameOver && currentGuess.length > 0) {
@@ -29,7 +30,31 @@ const handleDelete = () => {
 const handleEnter = () => {
   if (currentGuess.length === 5) {
     const userGuess = currentGuess.join('').toUpperCase()
-    if (secretWord === userGuess) {
+    let numCorrectLetters = 0
+    const guessState = []
+    for (let i = 0; i < userGuess.length; i++) {
+      if (userGuess[i] === secretWord[i]) {
+        guessState.push('correct')
+        numCorrectLetters++
+      } else if (secretWord.includes(userGuess[i])) {
+        guessState.push('included')
+      } else {
+        guessState.push('incorrect')
+      }
+    }
+    guesses.push(guessState)
+    const currentRow = guesses.length - 1
+    for (let i = 0; i < userGuess.length; i++) {
+      const currentDiv = document.getElementById(currentRow * 5 + i + 1)
+      if (guessState[i] === 'correct') {
+        currentDiv.style.backgroundColor = 'green'
+      } else if (guessState[i] === 'included') {
+        currentDiv.style.backgroundColor = 'yellow'
+      } else {
+        currentDiv.style.backgroundColor = 'grey'
+      }
+    }
+    if (numCorrectLetters === secretWord.length) {
       messageEl.innerText = 'You Win!'
       gameOver = true
     } else {
@@ -42,7 +67,6 @@ const handleEnter = () => {
 const handleClick = (event) => {
   const keyText = event.target.textContent
   const currentDiv = document.getElementById(currentLetter)
-
   if (keyText === 'Del') {
     handleDelete()
   } else if (keyText === 'Enter') {
@@ -60,3 +84,5 @@ const keyBoard = document.querySelectorAll('button')
 keyBoard.forEach((key) => {
   key.addEventListener('click', handleClick)
 })
+
+createGameBoard()
